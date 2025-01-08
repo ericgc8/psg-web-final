72150928dbb7a3528d96b1cc310970318b8f783c
ericgc8 <eric@ericgcam.com>
2025-01-08 12:54:12 -0500

    Fixed contact section state

--- src/components/ContactSection/ContactSection.tsx
+++ src/components/ContactSection/ContactSection.tsx
@@ -1,5 +1,5 @@
 // src/components/ContactSection/ContactSection.tsx
-import React, { useState } from 'react';
+import React from 'react';
 import Button from '../Button/Button';
 
 interface ContactSectionProps {
